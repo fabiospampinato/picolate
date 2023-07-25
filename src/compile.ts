4132b07a3eb6bb47288c5d9d300c3c20e1291894
Fabio Spampinato <spampinabio@gmail.com>
2023-07-25 14:20:04 +0100

    Renamed an internal function

--- src/compile.ts
+++ src/compile.ts
@@ -7,7 +7,7 @@ import parse from './parse';
 
 //TODO: This function is generated just to bail out of strict mode, which is needed just because "with" is used, maybe it could be done more nicely
 
-// const execute = new Function ( 'context', 'nodes', 'output', 'execute', /* javascript */`
+// const evaluate = new Function ( 'context', 'nodes', 'output', 'evaluate', /* javascript */`
 //   with ( context ) {
 //     for ( const node of nodes ) {
 //       const type = node.type;
@@ -18,21 +18,21 @@ import parse from './parse';
 //       } else if ( type === 'each.open' ) {
 //         const values = eval ( node.values );
 //         for ( const value of values ) {
-//           execute ( { ...context, [node.value]: value }, node.children, output, execute );
+//           evaluate ( { ...context, [node.value]: value }, node.children, output, evaluate );
 //         }
 //       } else if ( type === 'if.open' ) {
 //         if ( eval ( node.value ) ) {
-//           execute ( context, node.children, output, execute );
+//           evaluate ( context, node.children, output, evaluate );
 //         }
 //       } else if ( type === 'with.open' ) {
 //         const nodeContext = eval ( node.value );
-//         execute ( { ...context, ...nodeContext }, node.children, output, execute );
+//         evaluate ( { ...context, ...nodeContext }, node.children, output, evaluate );
 //       }
 //     }
 //   }
 // `);
 
-const execute = new Function ( 'c', 'n', 'o', 'e', `with(c)for(const m of n){const t=m.type;if(t==="eval")o.push(String(eval(m.value)));else if(t==="string")o.push(m.value);else if(t==="each.open"){const w=eval(m.values);for(const v of w)e({...c,[m.value]:v},m.children,o,e)}else if(t==="if.open")eval(m.value)&&e(c,m.children,o,e);else if(t==="with.open"){const mc=eval(m.value);e({...c,...mc},m.children,o,e)}}` );
+const evaluate = new Function ( 'c', 'n', 'o', 'e', `with(c)for(const m of n){const t=m.type;if(t==="eval")o.push(String(eval(m.value)));else if(t==="string")o.push(m.value);else if(t==="each.open"){const w=eval(m.values);for(const v of w)e({...c,[m.value]:v},m.children,o,e)}else if(t==="if.open")eval(m.value)&&e(c,m.children,o,e);else if(t==="with.open"){const mc=eval(m.value);e({...c,...mc},m.children,o,e)}}` );
 
 /* MAIN */
 
@@ -44,7 +44,7 @@ const compile = ( template: string ): (( context?: object ) => string) => {
 
     const output: string[] = [];
 
-    execute ( context, nodes, output, execute );
+    evaluate ( context, nodes, output, evaluate );
 
     return output.join ( '' );
 
